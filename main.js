348272b68015ca66dcb552a26fa06ac4167e3a7c
Akshita Swarnkar <88489579+Shanvie@users.noreply.github.com>
2022-09-19 15:41:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+img="";
+status="";
+objectDetector="";
+objects=[];
+
+function preload(){
+	img=loadImage("lapo.jpg");
+}
+
+function setup(){
+	canvas=createCanvas(380,380);
+	canvas.center();
+	video=createCapture(VIDEO);
+	video.size(350,350);
+	video.hide();
+}
+
+function start(){
+     objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+	document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded(){
+	console.log("modelLoaded");
+	status=true;
+}
+
+function gotResult(error,results){
+     if(error){
+     	console.log(error);
+     }
+     console.log(results);
+     objects=results;
+}
+
+function draw(){
+	image(video,0,0,350,350);
+
+if(status!=""){
+objectDetector.detect(video,gotResult);
+for (var i=0;i<objects.length;i++){
+	document.getElementById("status").innerHTML="Status: Objects Detected";
+	document.getElementById("number_of_objects").innerHTML="Number of objects detected: "+objects.length;
+r=random(255);
+g=random(255);
+b=random(255);
+	fill(r,g,b);
+	percent=floor(objects[i].confidence*100);
+	text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+	noFill();
+	stroke(r,g,b);
+	rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+}
+}
+}
